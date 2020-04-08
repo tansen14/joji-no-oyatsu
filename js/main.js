6d242b8647ce6cbe16c84a22aaeb39c1f63709f8
masumura <dmasumura@system-renovate.co.jp>
2020-04-08 17:34:06 +0900

    注文番号の取得を追加

--- js/main.js
+++ js/main.js
@@ -20,13 +20,15 @@
   }
 
   function onSubmitBtn() {
+    alert("test");
+
     // 結果送信ボタン
     const submitBtn = document.getElementById('submit-btn');
 
     event.stopPropagation();
     // イベントキャンセル
     event.preventDefault();
-    alert("test");
+
 
     // 送信確認表を表示
     result();
@@ -41,6 +43,7 @@
     const mail = "test@gmail.com";
     const address = "青森県弘前市〇〇－１";
     const phone = "090-1234-5678";
+    const sumPrice = 500;
     // itemの個数の配列
     const items = document.getElementsByName('counter');
 
@@ -48,9 +51,6 @@
     const confirm = document.getElementById('confirm');
     confirm.style.display = "block";
     pageScroll()
-
-
-
 
     // 裏に用意したGoogleFormにajax非同期通信で値を送信する
     $.ajax({
@@ -66,6 +66,7 @@
         'entry.545868401': mail,                // メールアドレス
         'entry.775530315': address,             // 住所
         'entry.1315798513': phone,              // 電話番号
+        'entry.1407908955': sumPrice,           // 合計料金
         'entry.12472245': items[0].value,       // ジョージのおやつHAPPY BOX
         'entry.1191119162': items[1].value,     // カヌレ
         'entry.1553621218': items[2].value,     // チーズケーキ
@@ -82,28 +83,36 @@
         // 送信が成功した場合
         0: function() {
           // 文字列を送信させる
-
+          getOrderNum(lineId);
           // LIFFを閉じる
-
+          liff.closeWindow();
         },
         200: function() {
-          // テキストに結果を代入
-          // scoreLabel.textContent = '送信に失敗しました。';
-          // ボタンのテキストを変更
-          // scoreSubmit.textContent = 'もう一度やり直す';
-          // ボタンのhrefを変更
-          // scoreSubmit.href = '';
+          //
         }
       }
     });
   }
 
-  // "dateにイベントリスナーでonchangeかな
-  $("#date").change(function(){
+  // 初回API読み込み
+  date_check();
+  // "dateにイベントリスナーでonchange
+  $("#date").change(function() {
+    date_check();
+  });
 
+  function date_check() {
+    $("#exampleFormControlSelect1").prop("disabled", true);
+    // 初期化
+    $("#exampleFormControlSelect1").empty();
+    const base_time = ['11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00'];
+    for(let i = 0; i < base_time.length - 1; i++){
+      let data_option = '<option value=' + base_time[i] + '>' + base_time[i] + ' 〜 ' + base_time[i+1] + '</option>';
+      $("#exampleFormControlSelect1").append(data_option);
+    }
     $("#exampleFormControlSelect1 option").prop("disabled", false);
 
-    let date = $(this).val().replace(/-/g, '');
+    let date = $("#date").val().replace(/-/g, '');
 
     // 裏に用意したGoogleFormにajax非同期通信で値を送信する
     $.ajax({
@@ -116,10 +125,19 @@
       reserved_dates.forEach(element => {
         $('#exampleFormControlSelect1 option[value="' + element + '"]').text('✕既にご予約されています。').prop("disabled", true);
       });
+      $("#exampleFormControlSelect1").prop("disabled", false);
     })
     .fail(function(data) {
-      //
+      $("#exampleFormControlSelect1").prop("disabled", false);
     });
-  });
+  }
 
+  function getOrderNum(lineId) {
+    fetch('https://script.google.com/macros/s/AKfycbzUlzd4eB6DCpcoksAmSif5d9APlVASv9wnoqmzEqSxtAtmT3px/exec?kind=order_num&userId=' + lineId)
+      .then((data) => data.json())
+      .then((obj) => {
+        console.log(obj.orderNum);
+        return "【注文番号】" + obj.oderNum;
+      });
+  }
 }
